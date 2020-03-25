843fa6362c718a4a5b3a908842a4bb09a6ca3e11
H0p303 <45872829+H0p303@users.noreply.github.com>
2020-03-25 22:44:31 +0100

    Nothing

--- Website/JS/Script.js
+++ Website/JS/Script.js
@@ -3,81 +3,94 @@ var activeFormType = 0;
 var firstSectionBG = 0;
 var secondSectionBG = 0;
 var thirdSectionBG = 0;
-    
-
 
 function OpenCloseForm() {
-
-    if (activeForm == 0){
-        document.getElementById("myLoginForm").style.display = "block";
-        activeForm = 1;
-    }
-    else{
-        document.getElementById("myLoginForm").style.display = "none";
-        document.getElementById("mySignupForm").style.display = "none";
-        activeFormType = 0;
-        activeForm = 0;
-    }
+  if (activeForm == 0) {
+    document.getElementById("myLoginForm").style.display = "block";
+    activeForm = 1;
+  } else {
+    document.getElementById("myLoginForm").style.display = "none";
+    document.getElementById("mySignupForm").style.display = "none";
+    activeFormType = 0;
+    activeForm = 0;
+  }
 }
 
-function OpenFormType(){
-    if(activeFormType == 0){
-        document.getElementById("mySignupForm").style.display = "block";
-        document.getElementById("myLoginForm").style.display = "none";
-        activeFormType = 1;
-    }
-    else{
-        document.getElementById("mySignupForm").style.display = "none";
-        document.getElementById("myLoginForm").style.display = "block";
-        activeFormType = 0;
-    }
+function OpenFormType() {
+  if (activeFormType == 0) {
+    document.getElementById("mySignupForm").style.display = "block";
+    document.getElementById("myLoginForm").style.display = "none";
+    activeFormType = 1;
+  } else {
+    document.getElementById("mySignupForm").style.display = "none";
+    document.getElementById("myLoginForm").style.display = "block";
+    activeFormType = 0;
+  }
 }
 
-  //======\\
- //Sections\\
+//======\\
+//Sections\\
 //==========\\
 
+function FirstSection() {
+  if (firstSectionBG == 0) {
+    firstSectionBG = 1;
+    document.getElementById("Card1").style.backgroundColor = "rgba(0,0,0,0.5)";
+    document.getElementById("Content1").style.display = "block";
 
-function FirstSection(){
-    if  (firstSectionBG == 0){
-        firstSectionBG = 1;
-        document.getElementById("Card1").style.backgroundColor = "rgba(0,0,0,0.5)";
-        document.getElementById("Content1").style.display = "block";
-    }
+    document.getElementById("Card2").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content2").style.display = "none";
 
-    else{
-        firstSectionBG = 0;
-        document.getElementById("Card1").style.backgroundColor = "rgba(0,0,0,0.18)";
-        document.getElementById("Content1").style.display = "none";
-    }
+    document.getElementById("Card3").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content3").style.display = "none";
+
+    secondSectionBG = 0;
+    thirdSectionBG = 0;
+  } else {
+    firstSectionBG = 0;
+    document.getElementById("Card1").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content1").style.display = "none";
+  }
 }
 
-function SecondSection(){
-    if  (secondSectionBG == 0){
-        secondSectionBG = 1;
-        document.getElementById("Card2").style.backgroundColor = "rgba(0,0,0,0.5)";
-        document.getElementById("Content2").style.display = "block";
+function SecondSection() {
+  if (secondSectionBG == 0) {
+    secondSectionBG = 1;
+    document.getElementById("Card2").style.backgroundColor = "rgba(0,0,0,0.5)";
+    document.getElementById("Content2").style.display = "block";
 
-    }
+    document.getElementById("Card1").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content1").style.display = "none";
 
-    else{
-        secondSectionBG = 0;
-        document.getElementById("Card2").style.backgroundColor = "rgba(0,0,0,0.18)";
-        document.getElementById("Content2").style.display = "none";
-    }
+    document.getElementById("Card3").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content3").style.display = "none";
+
+    firstSectionBG = 0;
+    thirdSectionBG = 0;
+  } else {
+    secondSectionBG = 0;
+    document.getElementById("Card2").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content2").style.display = "none";
+  }
 }
 
-function ThirdSection(){
-    if  (thirdSectionBG == 0){
-        thirdSectionBG = 1;
-        document.getElementById("Card3").style.backgroundColor = "rgba(0,0,0,0.5)";
-        document.getElementById("Content3").style.display = "block";
+function ThirdSection() {
+  if (thirdSectionBG == 0) {
+    thirdSectionBG = 1;
+    document.getElementById("Card3").style.backgroundColor = "rgba(0,0,0,0.5)";
+    document.getElementById("Content3").style.display = "block";
 
-    }
+    document.getElementById("Card1").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content1").style.display = "none";
 
-    else{
-        thirdSectionBG = 0;
-        document.getElementById("Card3").style.backgroundColor = "rgba(0,0,0,0.18)";
-        document.getElementById("Content3").style.display = "none";
-    }
+    document.getElementById("Card2").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content2").style.display = "none";
+
+    secondSectionBG = 0;
+    firstSectionBG = 0;
+  } else {
+    thirdSectionBG = 0;
+    document.getElementById("Card3").style.backgroundColor = "rgba(0,0,0,0.18)";
+    document.getElementById("Content3").style.display = "none";
+  }
 }
--- Website/PHP/Index.php
+++ Website/PHP/Index.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,7 +56,46 @@
     </section>
     <main>
 
-    <div class="form-popup" id="myLoginForm">
+
+
+<?php
+
+    // if(isset($_SESSION['userID'])){
+    //     echo'<div class="form-popup" id="myLoginForm">
+    //     <form action="../Includes/Login.inc.php" class="form-container" method="post">
+    //       <h1>Login</h1>
+    //       <label for="email"><b>Email</b></label>
+    //       <input type="text" placeholder="Enter Email" name="mailuid" required/>
+    //       <label for="psw"><b>Password</b></label>
+    //       <input type="password" placeholder="Enter Password" name="pwd" required/>
+    //       <button type="submit" class="btn" name="Login-Submit">Login</button>
+    //       <button type="button" class="btn" onclick="OpenFormType()">Sign Up</button>
+    //     </form>
+    //   </div>
+  
+    //   <div class="form-popup" id="mySignupForm">
+    //       <form method="post" class="form-container" action="../Includes/Signup.inc.php">
+    //           <h1>Sign Up</h1>
+    //           <input type="text" placeholder="Username" name="uid" required>
+    //           <input type="text" placeholder="Email" name="mail" required>
+    //           <input type="password" placeholder="Password" name="pwd" required>
+    //           <input type="password" placeholder="Re-type Password" name="pwd-verify" required>
+    //           <button type="submit" name="singup-submit" class="btn">Sign Up</button>
+    //           <button type="button" class="btn" onclick="OpenFormType()">Login</button>
+    //       </form>
+    //   </div>';
+    // }
+    // else{
+    //     echo'<div class="form-popup" id="myLougoutForm">
+    //     <form action="../Includes/Logout.inc.php" method="post">
+    //         <button type="submit">Logout</button>
+    //     </form>
+    // </div>';
+    // }
+
+?>
+
+    <!-- <div class="form-popup" id="myLoginForm">
       <form action="../Includes/Login.inc.php" class="form-container" method="post">
         <h1>Login</h1>
         <label for="email"><b>Email</b></label>
@@ -73,10 +117,24 @@
             <button type="submit" name="singup-submit" class="btn">Sign Up</button>
             <button type="button" class="btn" onclick="OpenFormType()">Login</button>
         </form>
+    </div> -->
+
+    <div class="form-popup" id="myLougoutForm">
+        <form action="../Includes/Logout.inc.php" method="post">
+            <button type="submit">Logout</button>
+        </form>
     </div>
 
     <Div class="MainContent" id="Content1">
-        <h1>Lorem ipsum dolor</h1>
+        <h1>Lorem 1 dolor</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam recusandae natus facilis quasi sunt nemo blanditiis atque maxime cumque dolorem! Et, aspernatur dignissimos sit optio voluptate neque quo alias ducimus unde, facilis iusto! Non dolor id eum velit ratione quasi fugiat dolorum necessitatibus quis. Nam sit doloremque odio repellat rerum quia illo molestias praesentium consequatur eum, suscipit vel ipsam voluptate deleniti, reiciendis repudiandae autem iusto earum consectetur. Perspiciatis autem aspernatur tenetur vel iusto numquam soluta, nihil non ratione, possimus fuga placeat dolores, exercitationem quod eum officiis nesciunt explicabo sequi minima dignissimos necessitatibus dolore? Maxime repudiandae quidem delectus incidunt numquam, officia error, veritatis amet dolore, eveniet cumque consequuntur! Adipisci temporibus impedit fugiat quidem ad eveniet, tenetur soluta possimus fuga deserunt error eos odit doloribus. Blanditiis minus vero exercitationem perspiciatis mollitia minima voluptate beatae, amet earum! Odio amet asperiores nostrum inventore itaque impedit maiores ipsa, deserunt, aut quas, libero eligendi quae in? Est alias voluptate ex nesciunt quae numquam commodi dignissimos voluptates repudiandae recusandae eum perspiciatis exercitationem quis doloremque aliquam aspernatur, expedita architecto hic accusamus? Aperiam, tenetur nam harum asperiores fuga a. Sed quas dolores accusantium soluta hic quaerat reiciendis officia vel nulla consequatur itaque fuga nobis repellendus asperiores incidunt sint voluptates, minus, iste ipsam? Mollitia ad, dolorum impedit molestias sunt tempore nostrum rerum facere, deleniti aliquid illum qui ab doloribus labore laborum ullam obcaecati earum placeat pariatur, iusto aperiam. Tempore repudiandae ratione praesentium ut at libero est deleniti explicabo amet. Pariatur vitae quibusdam vero nam minima ea aliquam libero totam id.</p>
+    </Div>
+    <Div class="MainContent" id="Content2">
+        <h1>Lorem 2 dolor</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam recusandae natus facilis quasi sunt nemo blanditiis atque maxime cumque dolorem! Et, aspernatur dignissimos sit optio voluptate neque quo alias ducimus unde, facilis iusto! Non dolor id eum velit ratione quasi fugiat dolorum necessitatibus quis. Nam sit doloremque odio repellat rerum quia illo molestias praesentium consequatur eum, suscipit vel ipsam voluptate deleniti, reiciendis repudiandae autem iusto earum consectetur. Perspiciatis autem aspernatur tenetur vel iusto numquam soluta, nihil non ratione, possimus fuga placeat dolores, exercitationem quod eum officiis nesciunt explicabo sequi minima dignissimos necessitatibus dolore? Maxime repudiandae quidem delectus incidunt numquam, officia error, veritatis amet dolore, eveniet cumque consequuntur! Adipisci temporibus impedit fugiat quidem ad eveniet, tenetur soluta possimus fuga deserunt error eos odit doloribus. Blanditiis minus vero exercitationem perspiciatis mollitia minima voluptate beatae, amet earum! Odio amet asperiores nostrum inventore itaque impedit maiores ipsa, deserunt, aut quas, libero eligendi quae in? Est alias voluptate ex nesciunt quae numquam commodi dignissimos voluptates repudiandae recusandae eum perspiciatis exercitationem quis doloremque aliquam aspernatur, expedita architecto hic accusamus? Aperiam, tenetur nam harum asperiores fuga a. Sed quas dolores accusantium soluta hic quaerat reiciendis officia vel nulla consequatur itaque fuga nobis repellendus asperiores incidunt sint voluptates, minus, iste ipsam? Mollitia ad, dolorum impedit molestias sunt tempore nostrum rerum facere, deleniti aliquid illum qui ab doloribus labore laborum ullam obcaecati earum placeat pariatur, iusto aperiam. Tempore repudiandae ratione praesentium ut at libero est deleniti explicabo amet. Pariatur vitae quibusdam vero nam minima ea aliquam libero totam id.</p>
+    </Div>
+    <Div class="MainContent" id="Content3">
+        <h1>Lorem 3 dolor</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam recusandae natus facilis quasi sunt nemo blanditiis atque maxime cumque dolorem! Et, aspernatur dignissimos sit optio voluptate neque quo alias ducimus unde, facilis iusto! Non dolor id eum velit ratione quasi fugiat dolorum necessitatibus quis. Nam sit doloremque odio repellat rerum quia illo molestias praesentium consequatur eum, suscipit vel ipsam voluptate deleniti, reiciendis repudiandae autem iusto earum consectetur. Perspiciatis autem aspernatur tenetur vel iusto numquam soluta, nihil non ratione, possimus fuga placeat dolores, exercitationem quod eum officiis nesciunt explicabo sequi minima dignissimos necessitatibus dolore? Maxime repudiandae quidem delectus incidunt numquam, officia error, veritatis amet dolore, eveniet cumque consequuntur! Adipisci temporibus impedit fugiat quidem ad eveniet, tenetur soluta possimus fuga deserunt error eos odit doloribus. Blanditiis minus vero exercitationem perspiciatis mollitia minima voluptate beatae, amet earum! Odio amet asperiores nostrum inventore itaque impedit maiores ipsa, deserunt, aut quas, libero eligendi quae in? Est alias voluptate ex nesciunt quae numquam commodi dignissimos voluptates repudiandae recusandae eum perspiciatis exercitationem quis doloremque aliquam aspernatur, expedita architecto hic accusamus? Aperiam, tenetur nam harum asperiores fuga a. Sed quas dolores accusantium soluta hic quaerat reiciendis officia vel nulla consequatur itaque fuga nobis repellendus asperiores incidunt sint voluptates, minus, iste ipsam? Mollitia ad, dolorum impedit molestias sunt tempore nostrum rerum facere, deleniti aliquid illum qui ab doloribus labore laborum ullam obcaecati earum placeat pariatur, iusto aperiam. Tempore repudiandae ratione praesentium ut at libero est deleniti explicabo amet. Pariatur vitae quibusdam vero nam minima ea aliquam libero totam id.</p>
     </Div>
 
